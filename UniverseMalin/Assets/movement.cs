11d8a30834078d344b2a004fd3c40b8f1c0dfe50
Kazuto1120 <117380810+Kazuto1120@users.noreply.github.com>
2023-09-18 17:49:38 -0400

    add features
    added jumping, sprinting, gravity and animation to the game

--- UniverseMalin/Assets/movement.cs
+++ UniverseMalin/Assets/movement.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    [SerializeField] float speed = 12f;
+    private float speed;
     public CharacterController player;
     public GameObject body;
-    private bool ismoving = false;
+    public float gravity = -9.81f;
+    public float jumpheight = 3f;
+    public float walkspeed = 5f;
+    
+    public Transform groundedCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask ground;
+    public bool isGrounded;
+
+    public KeyCode jumpkey = KeyCode.Space;
+    public KeyCode runkey = KeyCode.LeftShift;
+    
+    Vector3 velocity;
+
 
 
     private void Start()
@@ -16,6 +29,17 @@ public class movement : MonoBehaviour
     }
     private void Update()
     {
+        isGrounded = Physics.CheckSphere(groundedCheck.position, groundDistance, ground);
+        if (isGrounded)
+        {
+            body.GetComponent<Animator>().SetBool("jump", false);
+        }
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        
+
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         if(x != 0 || y != 0)
@@ -28,6 +52,26 @@ public class movement : MonoBehaviour
         }
         Vector3 move = transform.right * x + transform.forward * y;
 
+        if (Input.GetKey(runkey))
+        {
+            speed = 2 * walkspeed;
+            body.GetComponent<Animator>().SetBool("isrunning", true);
+        }
+        else
+        {
+            speed = walkspeed;
+            body.GetComponent<Animator>().SetBool("isrunning", false);
+        }
         player.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetKey(jumpkey) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpheight * -2f * gravity);
+            body.GetComponent<Animator>().SetBool("jump", true);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        player.Move(velocity * Time.deltaTime);
     }
 }
