3cae00dbdf10b3ae9ef085311adaedec2a8ed200
Kazuto1120 <117380810+Kazuto1120@users.noreply.github.com>
2023-10-05 15:06:10 -0400

    added feature
    added enemy health bar

--- UniverseMalin/Assets/enemeysystem.cs
+++ UniverseMalin/Assets/enemeysystem.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class enemeysystem : MonoBehaviour
 {
     public float health = 100;
+    public Slider slider;
+
     void Start()
     {
-        
+        slider.maxValue = health;
+        sethealth();
     }
 
     // Update is called once per frame
@@ -18,9 +22,14 @@ public class enemeysystem : MonoBehaviour
     public void takedamage(float x)
     {
         health = health - x;
+        sethealth();
         if(health <=0)
         {
             Destroy(gameObject);
         }
+    }
+    private void sethealth()
+    {
+        slider.value = health;
     }
 }
