a811bce6c74782cd5a448a85ffa3f222c0546bb7
Kazuto1120 <117380810+Kazuto1120@users.noreply.github.com>
2023-11-14 11:47:47 -0500

    finish up  game menu

--- My project (16)/Assets/camerafreemovement.cs
+++ My project (16)/Assets/camerafreemovement.cs
@@ -1,37 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class camerafreemovement : MonoBehaviour
 {
     public float sensitivity = 500f;
+    public float sensitivityMax = 2000f;
+    public Slider Slider;
     private float xRotation = 0f;
     private float yRotation = 0f;
-    public float x, y;
+    private float x, y;
+    private float mouseX, mouseY;
     public float speed =1f;
     public KeyCode jumpkey = KeyCode.Space;
     public KeyCode downkey = KeyCode.LeftShift;
+    public KeyCode escape = KeyCode.Escape;
+    public GameObject canvas;
+    public bool incanvas;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        Slider.maxValue = sensitivityMax;
+        Slider.value = sensitivity;
+        canvas.SetActive(false);
+        incanvas = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -60f, 60f);
-        yRotation += mouseX;
-        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
-        Vector3 move = transform.right * x + transform.forward * y;
-        transform.position += move*Time.deltaTime*speed;
+
+        if (!incanvas) 
+        { move();}
         if (Input.GetKey(jumpkey))
         {
             transform.position += Vector3.up * speed*Time.deltaTime;
@@ -40,5 +50,40 @@ public class camerafreemovement : MonoBehaviour
         {
             transform.position -= Vector3.up * speed * Time.deltaTime;
         }
+        if (Input.GetKeyDown(escape))
+        switchmenu();
+    }
+    public void switchmenu()
+    {
+        
+        if (incanvas == false)
+        {
+
+            incanvas = true;
+            Cursor.lockState = CursorLockMode.None;
+            canvas.SetActive(true);
+
+        }
+        else if (incanvas)
+        {
+
+            incanvas = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            canvas.SetActive(false);
+
+        }
+    }
+    public void move()
+    {
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -60f, 60f);
+        yRotation += mouseX;
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        Vector3 move = transform.right * x + transform.forward * y;
+        transform.position += move * Time.deltaTime * speed;
+    }
+    public void back()
+    {
+        SceneManager.LoadScene("lobby");
     }
 }
