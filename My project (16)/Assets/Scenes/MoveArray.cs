29a4d815eafe6f54913d0dd6fbde13848ebd9f04
bcjorgy <bcjorgy@gmail.com>
2023-11-22 21:12:17 -0500

    adding a script to make robot move randomly

--- My project (16)/Assets/Scenes/MoveArray.cs
+++ My project (16)/Assets/Scenes/MoveArray.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class MoveArray : MonoBehaviour
@@ -7,6 +8,8 @@ public class MoveArray : MonoBehaviour
 
     //creating gameobject list
     public List<GameObject> waypoints;
+    public float speed = 2;
+    public bool goBack = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +19,26 @@ public class MoveArray : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        int index = 0;
+        Vector3 destination = waypoints[index].transform.position;
+       Vector3 newPos = Vector3.MoveTowards(transform.position, waypoints[index].transform.position, speed * Time.deltaTime);
+        transform.position = newPos;
+
+        //creating logic for distance
+        float distance = Vector3.Distance(transform.position, destination); 
+        if(distance <- 0.07)
+        {
+            if (index < waypoints.Count - 1)
+            {
+                index++;
+            }
+            else
+            {
+                if (goBack)
+                {
+                    index = 0;
+                }
+            }
+        }
     }
 }
