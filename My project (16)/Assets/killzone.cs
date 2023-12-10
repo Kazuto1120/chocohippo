ddbd372ca1dc1e54ed25e27efaff61e06d9cfb16
andrewgil22 <andrewgilnu1@gmail.com>
2023-12-10 18:11:57 -0500

    killzone

---
+++ My project (16)/Assets/killzone.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            SceneManager.LoadScene("gameover");
+            Destroy(gameObject);
+        }
+    }
+}
