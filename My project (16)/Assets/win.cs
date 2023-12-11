858704c1225fbdf5342e22201c559bac21f1625e
andrewgil22 <andrewgilnu1@gmail.com>
2023-12-10 21:17:18 -0500

    win change

--- My project (16)/Assets/win.cs
+++ My project (16)/Assets/win.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class win : MonoBehaviour
 {
         private void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("Player")) 
+        {
+            SceneManager.LoadScene("Win");
+            Destroy(gameObject);
+        }
     }
-       
     
 }
